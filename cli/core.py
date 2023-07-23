b93d79e56dae924fae5255d64728101eddbec780
Navid <navidjalili3@gmail.com>
2023-07-23 18:10:52 +0330

    Add get_input

--- cli/core.py
+++ cli/core.py
@@ -5,11 +5,12 @@ from inquirer import List, prompt
 from constants.cli_constants import APP_NAME, YEAR, VERSION, APP_DESCRIPTION
 from constants.cli_theme import custom_theme
 
+console = Console(theme=custom_theme)
+
 
 def show_info() -> None:
     """Show basic Information about the App
     """
-    console = Console(theme=custom_theme)
     """Show basic information about the Word Frequency Counter."""
     console.print(Align.center(
         f"[bold yellow]{APP_NAME}[/] - [info]{YEAR}[/info][info] - [bold]V[/bold]{VERSION}[/info]\n"))
@@ -32,3 +33,10 @@ def prompt_user() -> dict[str, str]:
     ]
     answer: dict[str, str] = prompt(questions)
     return answer
+
+
+def get_input() -> str:
+    txt = input(': ')
+    if len(txt.strip()) == 0:
+        raise ValueError("Empty value is not allowed")
+    return txt
