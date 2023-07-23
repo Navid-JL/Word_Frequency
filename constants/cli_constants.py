d7b75c324a407a441dc00526dff454c2d65e39e7
Navid <navidjalili3@gmail.com>
2023-07-23 18:12:28 +0330

    Add constants for selectable commands

--- constants/cli_constants.py
+++ constants/cli_constants.py
@@ -5,3 +5,6 @@ APP_NAME = "Word Frequency Counter"
 YEAR = datetime.today().year
 VERSION = "1.0.0"
 APP_DESCRIPTION = "Enter a piece of text and I will display the frequency of each word in the text."
+MANUALLY = 'Enter manually'
+CLIPBOARD = 'Copy from clipboard'
+EXIT = 'Exit'
